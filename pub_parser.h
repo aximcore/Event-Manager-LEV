95238a3cd193045c74d88f90b3017ffddabafecb
AximCore <aximcore@gmail.com>
2017-11-13 20:11:15 +0100

    MongoDB insert pub documents

--- pub_parser.h
+++ pub_parser.h
@@ -34,10 +34,12 @@ class PubParser : public osmium::handler::Handler {
     mongocxx::client client{mongocxx::uri{"mongodb://localhost:27017"}};
     mongocxx::database database;
     mongocxx::collection collection;
+
+    bsoncxx::builder::stream::document bsonBuilder;
 public:
     PubParser()
         : database(client["event_manager"]),
-          collection(db["event_location"])
+          collection(database["event_location"])
     {}
 
     void node(const osmium::Node & currentNode) {
@@ -46,22 +48,76 @@ public:
         if (tags.has_key("amenity") && tags.has_key("name")) {
             const char * amenity = tags["amenity"];
             const char * name = tags["name"];
-
+            //std::cout << "out\t" << name << "\t" << amenity << std::endl;
             if (amenity && name
-                    && !(
-                        std::strcmp(amenity, "pub")
-                        || std::strcmp(amenity, "bar") // TODO etc
+                &&( !std::strcmp(amenity, "pub")
+                        || !std::strcmp(amenity, "bar")
+                        || !std::strcmp(amenity, "nightclub")
+                        || !std::strcmp(amenity, "cafe")
+                        || !std::strcmp(amenity, "restaurant")
+                        || !std::strcmp(amenity, "fast_food")
+                        || !std::strcmp(amenity, "theatre")
+                        || !std::strcmp(amenity, "arts_centre")
+                        || !std::strcmp(amenity, "biergarten")
+                        || !std::strcmp(amenity, "ice_cream")
+                        || !std::strcmp(amenity, "food_court")
+                        || !std::strcmp(amenity, "community_center")
+                        || !std::strcmp(amenity, "cinema")
+                        || !std::strcmp(amenity, "casino")
                         )) {
-                // TODO mysql insert
-            }
-        }
+                std::map<std::string, std::string> documents;
+                documents["name"] = name;
+                documents["amenity"] = amenity;
+                documents["point_wkt"] = m_factory.create_point(currentNode);
 
+                if (tags.has_key("addr:postcode")) {
+                    documents["postcode"] = tags["addr:postcode"];
+                }
+                if (tags.has_key("addr:city")) {
+                    documents["city"] = tags["addr:city"];
+                }
+                if (tags.has_key("addr:street")) {
+                    documents["street"] = tags["addr:street"];
+                }
+                if (tags.has_key("addr:housenumber")) {
+                    documents["housenumber"] = tags["addr:housenumber"];
+                }
+                if (tags.has_key("capacity")) {
+                    documents["capacity"] = tags["capacity"];
+                }
+                if (tags.has_key("email")) {
+                    documents["email"] = tags["email"];
+                }
+                if (tags.has_key("facebook")) {
+                    documents["facebook"] = tags["facebook"];
+                }
+                if (tags.has_key("opening_hours")) {
+                    documents["opening_hours"] = tags["opening_hours"];
+                }
+                if (tags.has_key("website")) {
+                    documents["website"] = tags["website"];
+                }
+                if (tags.has_key("wheelchair")) {
+                    documents["wheelchair"] = tags["wheelchair"];
+                }
+
+                for (auto & doc : documents) {
+                    bsonBuilder << doc.first << doc.second;
+                }
+
+                bsoncxx::document::value pubDocument = bsonBuilder << bsoncxx::builder::stream::finalize;
+
+                bsoncxx::stdx::optional<mongocxx::result::insert_one> result =
+                 collection.insert_one(pubDocument.view());
+                std::cout << name << "\t" << amenity << std::endl;
+            } // if amenity & name
+        } // if has_key
     }
 
     static void parse(std::string filePath) {
         osmium::handler::DynamicHandler handler;
         handler.set<PubParser>();
-        osmium::io::Reader inputFile{filePath, osmium::osm_entity_bits::node};
+        osmium::io::Reader inputFile{filePath, osmium::osm_entity_bits::node | osmium::osm_entity_bits::way};
         osmium::apply(inputFile, handler);
     }
 };
