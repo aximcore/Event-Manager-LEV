505c5b3feca68e2bd7a584666b6af23f799e7f2b
nestquick <nestquick@me.com>
2017-11-14 20:43:54 +0100

    Update project to Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,9 @@ import { routing }        from './app.routing';
 import { AlertComponent } from './_directives/index';
 import { AuthGuard } from './_guards/index';
 import { AlertService, AuthenticationService, UserService } from './_services/index';
-import { HomeComponent } from './home/index';
-import { LandingComponent } from './landing/index';
-import { LoginComponent } from './login/index';
-import { RegisterComponent } from './register/index';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
     imports: [
@@ -31,7 +30,6 @@ import { RegisterComponent } from './register/index';
         AlertComponent,
         HomeComponent,
         LoginComponent,
-        LandingComponent,
         RegisterComponent
     ],
     providers: [
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,19 +1,17 @@
 ﻿import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/index';
-import { LoginComponent } from './login/index';
-import { LandingComponent } from './landing/index';
-import { RegisterComponent } from './register/index';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './_guards/index';
 
 const appRoutes: Routes = [
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
-    { path: 'landing', component: LandingComponent},
     { path: 'register', component: RegisterComponent },
 
     // otherwise redirect to home
-    { path: '**', redirectTo: 'landing' }
+    { path: '**', redirectTo: 'home' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
