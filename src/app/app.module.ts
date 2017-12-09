89d3bac868d497e8f537afa0d6b601d6c57baacc
nestquick <nestquick@me.com>
2017-12-09 21:39:13 +0100

    init: add source files

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+﻿import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import {AppComponent} from './app.component';
+import {routing} from './app.routing';
+import {HomeComponent} from './home/home.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule {
+}
---
+++ src/app/app.routing.ts
@@ -0,0 +1,11 @@
+﻿import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+
+const appRoutes: Routes = [
+    {
+        path: '', component: HomeComponent, children: []
+    },
+    {path: '**', redirectTo: ''}
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
