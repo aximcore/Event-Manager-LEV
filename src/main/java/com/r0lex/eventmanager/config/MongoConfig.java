d5358474e21ca7f95611ffdbbd92a995ceed9c43
nestquick <nestquick@me.com>
2017-12-12 22:19:28 +0100

    Add mongoDb logging

--- src/main/java/com/r0lex/eventmanager/config/MongoConfig.java
+++ src/main/java/com/r0lex/eventmanager/config/MongoConfig.java
@@ -3,8 +3,10 @@ package com.r0lex.eventmanager.config;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import com.r0lex.eventmanager.repository.LocationsRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.core.mapping.event.LoggingEventListener;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @Configuration
@@ -13,6 +15,11 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration{
     @Override
     protected String getDatabaseName() {
         return "event_manager";
+    }
+
+    @Bean
+    public LoggingEventListener mongoEventListener() {
+        return new LoggingEventListener();
     }
 
     @Override
