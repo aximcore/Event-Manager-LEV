ebc8ad849438cee8ff44838c4e6e0189b6d98f12
nestquick <nestquick@me.com>
2017-12-17 15:41:12 +0100

    Add getCurrentUser endpoint

--- src/main/java/com/r0lex/eventmanager/controller/AuthController.java
+++ src/main/java/com/r0lex/eventmanager/controller/AuthController.java
@@ -5,6 +5,9 @@ import com.r0lex.eventmanager.model.UserCredential;
 import com.r0lex.eventmanager.service.TokenService;
 import com.r0lex.eventmanager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -36,6 +39,13 @@ public class AuthController {
         }
 
         return Mono.empty();
+    }
+
+    @GetMapping("/user")
+    Mono<User> getCurrentUser(
+            @RequestParam String token
+    ) {
+        return this.userService.loadUserByToken(token);
     }
 
 }
