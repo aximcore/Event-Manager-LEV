51c75bda39abb01897f0372410f84693ee6e8ba8
Adam Tamics <kacshuffle@gmail.com>
2017-12-19 05:55:20 +0100

    feat(AdminEventsEdit): add snackbar

--- src/app/admin/admin-events/edit/admin-events-edit.component.ts
+++ src/app/admin/admin-events/edit/admin-events-edit.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit, Output} from '@angular/core';
 import {EventObject} from '../../../model';
 import {EventService} from '../../../service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
     templateUrl: './admin-events-edit.component.html',
@@ -13,21 +14,23 @@ export class AdminEventsEditComponent implements OnInit {
     @Output()
     public event: EventObject = <EventObject>{};
 
-    constructor(private eventService: EventService, private route: ActivatedRoute) {
+    constructor(private eventService: EventService, private route: ActivatedRoute, private snackBar: MatSnackBar, private router: Router) {
     }
 
     ngOnInit() {
         this.route.params.subscribe(params => {
             this.eventService.findById(params['id']).subscribe(event => {
-                if(event) {
+                if (event) {
                     this.event = event;
                 }
             });
-        })
+        });
     }
 
     onSubmit() {
         this.eventService.save(this.event);
+        this.snackBar.open('Successfully saved!', '', {duration: 2000, horizontalPosition: 'center'});
+        this.router.navigate(['admin', 'events']);
     }
 
 }
